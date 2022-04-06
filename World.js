372e5a6954d857330a81a2d6fdd46104dc5562a9
Tolmeros@github.com <test>
2022-04-07 02:47:16 +0300

    Intermediate saves

--- World.js
+++ World.js
@@ -1,6 +1,8 @@
 
 module.exports = class World {
   #population;
+  #intevalUpdater;
+  #worldTime = 0;
 
   constructor() {
     this.#population = [];
@@ -11,12 +13,40 @@ module.exports = class World {
     // взросление 
   }
 
-  addHuman(human) {
-    this.#population.push(human);
+  addCreature(creature) {
+    this.#population.push(creature);
   }
 
   populate() {
     
+  }
+
+  timeStep(years=1) {
+    //console.log(`World.timeStep: years = ${years}`);
+    this.#worldTime += years;
+    //console.log(`World.timeStep: current world year ${this.#worldTime}`);
+
+    
+  }
+
+  run() {
+    let t = this;
+    this.#intevalUpdater = setInterval(function() {
+      t.timeStep();
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.#intevalUpdater);
+  }
+
+  infoText() {
+    let result = ["Info :"];
+
+    result.push(`Current world time: ${this.#worldTime}`);
+    result.push(`Current world population: ${this.#population.length}`);
+
+    return result.join('\n');
   }
 
 }
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const process = require('process');
+
 const World = require('./World.js');
 const BugmonMale = require('./BugmonMale.js');
 const BugmonFemale = require('./BugmonFemale.js');
@@ -5,3 +7,19 @@ const BugmonFemale = require('./BugmonFemale.js');
 const firstWorld = new World();
 const firstBugmonMale = new BugmonMale();
 const firstBugmonFemale = new BugmonFemale();
+
+firstWorld.addCreature(firstBugmonMale);
+firstWorld.addCreature(firstBugmonFemale);
+
+firstWorld.run();
+
+intervalInfoPrint = setInterval(function() {
+  console.log(firstWorld.infoText());
+}, 1000);
+
+process.on('SIGINT', () => {
+  firstWorld.stop();
+  clearInterval(intervalInfoPrint);
+});
+
+
